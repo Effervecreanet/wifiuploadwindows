d9dbb13d22641bee31e475bc37b93ee4c7a9412d
Franck Lesage <140308388+Effervecreanet@users.noreply.github.com>
2024-04-19 09:13:47 +0200

    fill wu_content.c

--- src/wu_content.c
+++ src/wu_content.c
@@ -1,23 +1,37 @@
+#include "wu_content.h"
 
 
-const struct http_resource http_resources[] = {
-  {"accueil", "text/html", "html\\light\\fr\\accueil", "html\\light\\en\\accueil", "html\\dark\\fr\\accueil", "html\\dark\\en\\accueil"},
-  {"credits", "text/html"},
-  {"favicon.ico", "image/png"},
-  {"images/ewu_bg.png", "image/png"},
-  {"images/ewu_checkedfolder.png", "image/png"},
-  {"images/ewu_openfolder.png", "image/png"},
-  {"images/ewu_quit.png", "image/png"},
-  {"images/ewu_welcome.png", "image/png"},
-  {"images/ewu_send.png", "image/png"},
-  {"images/ewu_ticksuccess.png", "image/png"},
-  {"images/ewu_tickwrong.png", "image/png"},
-  {"images/ewu_top_bg.png", "image/png"},
-  {"images/logo4_effervecreanet.png", "image/png"},
-  {"erreur_fichier_nul", "text/html"},
-  {"succes", "text/html"},
-  {"quit", "text/html"},
-  {"openRep", "text/html"},
-  {"erreur_404", "text/html"},
-  {"", ""}
+
+const struct _http_resources_pages http_resources_pages[] = {
+  {"index", "text/html", "html\\light\\fr\\index", "html\\dark\\fr\\index"},
+  {"credits", "text/html", "html\\light\\fr\\credits", "html\\dark\\fr\\credits"},
+  {"settings", "text/html", "html\\light\\fr\\settings", "html\\dark\\fr\\settings"},
+  {"success", "text/html", "html\\light\\fr\\success", "html\\dark\\fr\\success"}};
+
+const struct _http_resources_images http_resources_images[] = {
+  {"favicon.ico", "image/x-icon"},
+  {"images/wu_images_light_theme/ewu_bg.png", "image/png"},
+  {"images/wu_images_light_theme/ewu_checkedfolder.png", "image/png"},
+  {"images/wu_images_light_theme/ewu_openfolder.png", "image/png"},
+  {"images/wu_images_light_theme/ewu_quit.png", "image/png"},
+  {"images/wu_images_light_theme/ewu_welcome.png", "image/png"},
+  {"images/wu_images_light_theme/ewu_send.png", "image/png"},
+  {"images/wu_images_light_theme/ewu_ticksuccess.png", "image/png"},
+  {"images/wu_images_light_theme/ewu_tickwrong.png", "image/png"},
+  {"images/wu_images_light_theme/ewu_top_bg.png", "image/png"},
+  {"images/wu_images_light_theme/logo4_effervecreanet.png", "image/png"},
+  {"images/wu_images_dark_theme/ewu_bg.png", "image/png"},
+  {"images/wu_images_dark_theme/ewu_checkedfolder.png", "image/png"},
+  {"images/wu_images_dark_theme/ewu_openfolder.png", "image/png"},
+  {"images/wu_images_dark_theme/ewu_quit.png", "image/png"},
+  {"images/wu_images_dark_theme/ewu_welcome.png", "image/png"},
+  {"images/wu_images_dark_theme/ewu_send.png", "image/png"},
+  {"images/wu_images_dark_theme/ewu_ticksuccess.png", "image/png"},
+  {"images/wu_images_dark_theme/ewu_tickwrong.png", "image/png"},
+  {"images/wu_images_dark_theme/ewu_top_bg.png", "image/png"},
+  {"images/wu_images_dark_theme/logo4_effervecreanet.png", "image/png"}};
+
+const char http_resource_cmd[2][16] = {
+  {"quit"},
+  {"openRep"},
 };
--- src/wu_content.h
+++ src/wu_content.h
@@ -1,4 +1,11 @@
-struct http_resource {
-  unsigned char resource[HTTP_RESSOURCE_MAX_LENGTH];
-  unsigned char type[HTTP_TYPE_MAX_LENGTH];
+struct _http_resources_pages {
+    char resource[64];
+    char type[24];
+    char path_theme1[64];
+    char path_theme2[64];
+};
+
+struct _http_resources_images {
+    char path[64];
+    char type[24];
 };
