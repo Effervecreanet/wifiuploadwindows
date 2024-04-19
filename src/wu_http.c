f45fef5a7ad3bf0e37cc81e912bc858993c4b48e
Franck Lesage <140308388+Effervecreanet@users.noreply.github.com>
2024-04-19 15:46:52 +0200

    copy serv_http_resource

--- src/wu_http.c
+++ src/wu_http.c
@@ -1,7 +1,8 @@
 #include <Windows.h>
 
+#include "wu_http_nv.h"
+#include "wu_http_receive.h"
 #include "wu_http.h"
-#include "wu_http_nv.c"
 
 #define HTTP_METHOD_GET    "GET"
 #define HTTP_METHOD_POST   "POST"
@@ -107,5 +108,166 @@ http_recv_headernv(struct header_nv* httpnv, int s) {
       return EINVAL;
   }
 
+  return 0;
+}
+
+int
+http_serv_resource(struct http_resource *res, int s,
+                   struct success_info *successinfo) {
+  HANDLE hFile;
+  DWORD fsize, read, err;
+  DWORD BufferUserNameSize = 254;
+  SYSTEMTIME sysTime;
+  struct header_nv httpnv[HEADER_NV_MAX_SIZE];
+  char *pbufferin = NULL, *pbufferout = NULL;
+  char BufferUserName[254];
+  size_t pbufferoutlen = 0;
+  char hrmn[6];
+  char *plastBS;
+  int ret;
+
+  ret = send(s, HTTP_VERSION, sizeof(HTTP_VERSION) - 1, 0);
+  if (ret != sizeof(HTTP_VERSION) - 1)
+    return -1;
+
+  if (send(s, " ", 1, 0) != 1)
+    return -1;
+
+  ret = send(s, HTTP_CODE_STATUS_OK_STR, sizeof(HTTP_CODE_STATUS_OK_STR) - 1, 0);
+  if (ret != sizeof(HTTP_CODE_STATUS_OK_STR) - 1)
+    return -1;
+
+  if (send(s, " ", 1, 0) != 1)
+    return -1;
+
+  ret = send(s, HTTP_STRING_STATUS_OK, sizeof(HTTP_STRING_STATUS_OK) - 1, 0);
+  if (ret != sizeof(HTTP_STRING_STATUS_OK) - 1)
+    return -1;
+
+  ret = send(s, "\r\n", 2, 0);
+  if (ret != 2)
+    return -1;
+
+  hFile = CreateFile(res->resource, GENERIC_READ, 0, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, 0);
+  if (hFile == INVALID_HANDLE_VALUE)
+    return GetLastError();
+
+  fsize = GetFileSize(hFile, NULL);
+  
+  if (strcmp(res->type, "text/html") == 0) {
+    ZeroMemory(BufferUserName, 254);
+    GetUserNameA(BufferUserName, &BufferUserNameSize);
+
+    pbufferin = (char*)malloc(fsize + 1);
+
+    ZeroMemory(pbufferin, fsize + 1);
+    ReadFile(hFile, pbufferin, fsize, &read, NULL);
+    if (read != fsize) {
+      err = GetLastError();
+      free(pbufferin);
+      CloseHandle(hFile);
+      return err;
+    }
+
+    ZeroMemory(&sysTime, sizeof(SYSTEMTIME));
+    GetLocalTime(&sysTime);
+    ZeroMemory(hrmn, 6);
+    if (sysTime.wHour < 10 && sysTime.wMinute < 10)
+      StringCchPrintf(hrmn, 6, "0%hu:0%hu", sysTime.wHour, sysTime.wMinute);
+    else if (sysTime.wHour < 10)
+      StringCchPrintf(hrmn, 6, "0%hu:%hu", sysTime.wHour, sysTime.wMinute);
+    else if (sysTime.wMinute < 10)
+      StringCchPrintf(hrmn, 6, "%hu:0%hu", sysTime.wHour, sysTime.wMinute);
+    else
+      StringCchPrintf(hrmn, 6, "%hu:%hu", sysTime.wHour, sysTime.wMinute);
+
+    plastBS = strrchr(res->resource, '\\') + 1;
+    if (strcmp(plastBS, "accueil") == 0 || strcmp(plastBS, "erreur_fichier_nul") == 0) {
+      pbufferout = (char*)malloc(fsize + BufferUserNameSize + 6);
+      ZeroMemory(pbufferout, fsize + BufferUserNameSize + 6);
+      StringCchPrintfA(pbufferout, fsize + BufferUserNameSize + 6 - 1, pbufferin, BufferUserName, hrmn);
+      free(pbufferin);
+      pbufferoutlen = strlen(pbufferout);
+    }
+    else if (strcmp(plastBS, "succes") == 0) {
+      pbufferout = (char*)malloc(fsize + 254 + 6);
+      ZeroMemory(pbufferout, fsize + 254 + 6);
+      StringCchPrintfA(pbufferout, fsize + 254 + 6,
+        pbufferin,
+        successinfo->filename,
+        successinfo->filenameSize,
+        successinfo->elapsedTime,
+        successinfo->averagespeed,
+        hrmn);
+      free(pbufferin);
+      pbufferoutlen = strlen(pbufferout);
+    }
+    else if (strcmp(plastBS, "credits") == 0 || strcmp(plastBS, "erreur_404") == 0) {
+      pbufferout = (char*)malloc(fsize + 6);
+      ZeroMemory(pbufferout, fsize + 6);
+      StringCchPrintfA(pbufferout, fsize + 6, pbufferin, hrmn);
+      free(pbufferin);
+      pbufferoutlen = strlen(pbufferout);
+    }
+    else {
+      free(pbufferin);
+      goto err;
+    }
+
+    ZeroMemory(httpnv, sizeof(struct header_nv) * HEADER_NV_MAX_SIZE);
+    create_http_header_nv(res, httpnv, pbufferoutlen);
+
+    ret = send_http_header_nv(httpnv, s);
+    if (ret > 0) {
+      if (pbufferout)
+        free(pbufferout);
+      CloseHandle(hFile);
+      return 8;
+    }
+
+    if (send(s, pbufferout, (int)pbufferoutlen, 0) <= 0) {
+      free(pbufferout);
+      CloseHandle(hFile);
+      return 10;
+    }
+
+    ret = 0;
+    free(pbufferout);
+err:
+    CloseHandle(hFile);
+  }
+  else {
+    char buffer[1024];
+
+    ZeroMemory(httpnv, sizeof(struct header_nv) * HEADER_NV_MAX_SIZE);
+    create_http_header_nv(res, httpnv, fsize);
+
+    ret = send_http_header_nv(httpnv, s);
+    if (ret > 0) {
+      CloseHandle(hFile);
+      return 11;
+    }
+
+    while (ReadFile(hFile, buffer, 1024, &read, NULL)) {
+      send(s, buffer, read, 0);
+      if (read < 1024)
+        break;
+    }
+
+    CloseHandle(hFile);
+    ret = 1;
+  }
+
+  return ret;
+}
+
+errno_t
+redir404(int s) {
+  struct http_resource ewures;
+
+  ZeroMemory(&ewures, sizeof(struct http_resource));
+  memcpy_s(&ewures, sizeof(struct http_resource), &http_resources[16], sizeof(struct http_resource));
+  http_serv_resource(&ewures, s, NULL);
+  
   return 0;
 }
