7df6db2ee0e8559e55fa10dc876da38ae0d81198
Franck Lesage <140308388+Effervecreanet@users.noreply.github.com>
2024-04-24 18:01:48 +0200

    add win32 compile script

---
+++ src/make_en.bat
@@ -0,0 +1,3 @@
+@echo off
+echo cl wu_msg.c wu_content.c wu_http_loop.c wu_http_nv.c wu_http_receive.c wu_http_theme.c wu_socket.c wu_http.c wu_main.c /Fe:wifiupload_en.exe
+cl wu_msg.c wu_content.c wu_http_loop.c wu_http_nv.c wu_http_receive.c wu_http_theme.c wu_socket.c wu_http.c wu_main.c /Fe:wifiupload_en.exe
