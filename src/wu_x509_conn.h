e73b743355abe9d2b7a5150a7fd6e62603200529
Franck Lesage <140308388+Effervecreanet@users.noreply.github.com>
2025-07-18 08:42:52 +0200

    rm wu_x509*

--- src/wu_x509_conn.h
+++
@@ -1,6 +0,0 @@
-int genKey(HCRYPTPROV *hProv, HCRYPTKEY *hKey);
-
-int getCertName(CERT_NAME_BLOB *SubjectBlob, BYTE pbEncodedName[128], DWORD *cbEncodedName);
-
-PCCERT_CONTEXT createCertSelfSign(BYTE ipAddr[4], CERT_NAME_BLOB *SubjectBlob, HCRYPTPROV hProv);
-
