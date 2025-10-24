e854bdf2879e274cb0b34069647dc812df84ae53
Franck Lesage <140308388+Effervecreanet@users.noreply.github.com>
2025-10-24 14:42:06 +0200

    Comment make_fr.bat and make_en.bat

--- src/make_fr.bat
+++ src/make_fr.bat
@@ -1,3 +1,5 @@
 @echo off
+REM This script should be run in x64 Native Tools Command prompt
+REM You have to run "rc resource.rc" to have "resource.res" file
 echo cl /DVERSION_FR wu_msg.c wu_content.c wu_http_loop.c wu_http_nv.c wu_http_receive.c wu_http_theme.c wu_socket.c wu_http.c wu_available_address.c wu_log.c wu_main.c resource.res /Fe:wifiupload_fr.exe
 cl /DVERSION_FR wu_msg.c wu_content.c wu_http_loop.c wu_http_nv.c wu_http_receive.c wu_http_theme.c wu_socket.c wu_http.c wu_available_address.c wu_log.c wu_main.c resource.res /Fe:wifiupload_fr.exe
