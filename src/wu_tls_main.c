743b53d05a6400635f43c71490fccbaf96c36126
Franck Lesage <140308388+Effervecreanet@users.noreply.github.com>
2025-12-06 10:25:32 +0100

    Write https_quit_wu()

--- src/wu_tls_main.c
+++ src/wu_tls_main.c
@@ -27,6 +27,11 @@
 #include "wu_tls_https.h"
 
 extern FILE* g_fphttpslog;
+extern FILE* g_fplog;
+extern int *g_usersocket;
+extern int *g_listensocket;
+extern HANDLE g_hNewFile_tmp;
+extern char* g_sNewFile_tmp;
 extern const struct _http_resources http_resources[];
 extern HANDLE g_hConsoleOutput;
 extern int g_tls_firstsend;
@@ -63,6 +68,32 @@ https_wu_quit_response(COORD cursorPosition[2], struct header_nv* httpnv, int* t
 	return;
 }
 
+static void
+https_quit_wu(int s_clt) {
+	if (g_fplog != NULL)
+		fclose(g_fplog);
+	if (g_fphttpslog != NULL)
+		fclose(g_fphttpslog);
+	if (g_usersocket != NULL && *g_usersocket != 0)
+		closesocket(*g_usersocket);
+	if (g_listensocket != NULL)
+		closesocket(*g_listensocket);
+	if (g_hConsoleOutput != INVALID_HANDLE_VALUE)
+		CloseHandle(g_hConsoleOutput);
+	if (g_hNewFile_tmp != INVALID_HANDLE_VALUE) {
+		CloseHandle(g_hNewFile_tmp);
+		DeleteFileA((LPCSTR)g_sNewFile_tmp);
+	}
+	if (g_credHandle != NULL && g_ctxtHandle != NULL &&
+		g_tls_sclt != NULL)
+		tls_shutdown(g_credHandle, g_ctxtHandle, *g_tls_sclt);
+	if (encryptBuffer != NULL)
+		free(encryptBuffer);
+	WSACleanup();
+	ExitProcess(TRUE);
+
+	return;
+}
 DWORD WINAPI wu_tls_loop(struct paramThread* prThread)
 {
 	BYTE pbEncodedName[128];
@@ -206,7 +237,7 @@ next_req:
 			}
 			else if (strcmp(reqline.resource + 1, "quit") == 0) {
 				https_wu_quit_response(&prThread->cursorPosition, headernv, &theme, s_clt, &bytesent);
-				
+				https_quit_wu(s_clt);
 			}
 			else {
 				check_cookie_theme(headernv, &theme);
