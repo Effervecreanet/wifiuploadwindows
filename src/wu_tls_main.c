91277061ee7445ab895a8dc3c315f064704ec071
Franck Lesage <140308388+Effervecreanet@users.noreply.github.com>
2026-04-08 21:34:32 +0200

    https_quit_wu close https user socket

--- src/wu_tls_main.c
+++ src/wu_tls_main.c
@@ -132,6 +132,8 @@ https_quit_wu(void) {
 		closesocket(g_usersocket);
 	if (g_listenhttpssocket)
 		closesocket(g_listenhttpssocket);
+	if (g_tls_sclt)
+		closesocket(g_tls_sclt);
 	if (g_hConsoleOutput != INVALID_HANDLE_VALUE)
 		CloseHandle(g_hConsoleOutput);
 	if (g_hNewFile_tmp != INVALID_HANDLE_VALUE) {
